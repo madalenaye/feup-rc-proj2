280bef44882f1e35ed6373fa5a3b6ab90b7c91c5
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-12-11 15:01:19 +0000

    start download.c

---
+++ download/download.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+int main(int argc, char * argv[]){
+
+    char hostname[200] = {"netlab1.fe.up.pt"};
+    printf("%d\n", argc);
+    printf("Hello world!");
+    return 0;
+}
